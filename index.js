b6c50b446e71041eb5f8f43c3465f34b6f99b7d1
prats7 <pratyushvishal04@gmail.com>
2020-12-27 23:12:06 +0530

    Fetch hero from APIs

---
+++ index.js
@@ -0,0 +1,182 @@
+const url = `https://www.superheroapi.com/api.php/10219177700206566/`;
+const searchBox = document.getElementById('name');
+const searchList = document.getElementById('search-result');
+let searchResults = [];
+const SEARCH_TEXT_LIMIT = 2;
+const loader = document.getElementById('loader');
+
+
+function initializeSuperHeroApp() {
+
+    document.addEventListener('click', function (event) {
+
+        const target = event.target;
+
+    if (target.classList.contains('add-to-fav')) {
+      // Find the hero data and store it in favourites and localstorage
+      const searchResultClickedId = target.dataset.id;
+      const hero = searchResults.filter(
+        (hero) => hero.id === searchResultClickedId
+      );
+      addHeroToFavourites(hero[0]);
+      renderSearchResults();
+    } else if (target.classList.contains('remove-from-fav')) {
+      // Find the hero data and remove from local storage
+      const searchResultClickedId = target.dataset.id;
+
+      // Show add to fav button and hide the remove from fav button
+      const addToFavBtn = document.querySelector(
+        `button[data-id="${searchResultClickedId}"].add-to-fav`
+      );
+      if (addToFavBtn) addToFavBtn.style.display = 'block';
+
+      const removeFromFavBtn = document.querySelector(
+        `button[data-id="${searchResultClickedId}"].remove-from-fav`
+      );
+      if (removeFromFavBtn) removeFromFavBtn.style.display = 'none';
+
+      removeHeroFromFavourites(searchResultClickedId);
+    }
+    });
+
+    searchBox.addEventListener('keyup', async function (event) {
+
+
+        if (event.target.value.length <= SEARCH_TEXT_LIMIT) {
+            searchList.innerHTML = '';
+            searchResults = [];
+            return;
+        }
+
+        // Show loader and remove existing search results
+
+        loader.style.display = 'block';
+
+        searchList.innerHTML = '';
+        searchResults = [];
+
+        try {
+            const data = await apiRequest(`${url}/search/${event.target.value}`);
+            loader.style.display = 'none';
+
+            if (data.success) {
+                searchResults = data.data.results;
+                renderSearchResults();
+            }
+        } catch (error) {
+            console.log('error', error);
+            loader.style.display = 'none';
+        }
+    });
+
+    return{
+        initializeSuperHeroApp
+    };
+
+}
+
+
+
+function renderSearchResults() {
+    // If data is empty warn the user
+    if (!searchResults || searchResults.length === 0) {
+        searchList.innerHTML = '<li class="no-results">No results found!</li>';
+        return;
+    }
+
+    const favSuperHeroes = getFavouriteSuperheroes();
+    searchList.innerHTML = '';
+
+    // Append each search result in the list
+    searchResults.forEach((element) => {
+        const li = document.createElement('li');
+        // Find if superhero exists in favourites
+        const indexOfSuperHeroInFavourites = favSuperHeroes.findIndex(
+            (hero) => hero.id === element.id
+        );
+        li.classList.add('search-result');
+        li.innerHTML = `
+                      <div class="search-left">
+                        <img src=${element.image.url} alt="" />
+                      </div>
+                      <div class="search-right">
+                        <a href="superhero.html?id=${element.id}">
+                          <div class="name">${element.name}</div>
+                        </a>
+                        <div class="full-name">${element.biography['full-name']
+            }</div>
+  
+                        <div class="address">${element.biography['place-of-birth']
+            }</div>
+                        <button class="btn add-to-fav" data-id=${element.id
+            } style="display: ${indexOfSuperHeroInFavourites === -1 ? 'block' : 'none'
+            }">Add to favourites</button>
+                        <button class="btn remove-from-fav" data-id=${element.id
+            }  style="display: ${indexOfSuperHeroInFavourites === -1 ? 'none' : 'block'
+            }">Remove from favourites</button>
+                      </div>
+                    `;
+        searchList.appendChild(li);
+    });
+}
+
+/* Send api requests */
+async function apiRequest(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        return {
+            data,
+            success: true,
+        };
+    } catch (error) {
+        console.log('error', error);
+        return {
+            error: error.message,
+            success: false,
+        };
+    }
+}
+
+// function removeHeroFromFavourites(heroId) {
+//     if (!heroId) return;
+
+//     let favouritesFromLocalStorage = getFavouriteSuperheroes();
+
+//     // Remove hero from localstorage
+//     favouritesFromLocalStorage = favouritesFromLocalStorage.filter(
+//       (item) => item.id !== heroId
+//     );
+
+//     // Save in localstorage
+//     localStorage.setItem(
+//       FAVOURITES,
+//       JSON.stringify(favouritesFromLocalStorage)
+//     );
+
+//   }
+
+  // function addHeroToFavourites(hero) {
+  //   if (!hero) return;
+
+  //   const favouritesFromLocalStorage = getFavouriteSuperheroes();
+  //   favouritesFromLocalStorage.push(hero);
+
+  //   // Save in localstorage
+  //   localStorage.setItem(
+  //     FAVOURITES,
+  //     JSON.stringify(favouritesFromLocalStorage)
+  //   );
+
+  // }
+
+
+  // function getFavouriteSuperheroes() {
+  //   return localStorage.getItem(FAVOURITES)
+  //     ? JSON.parse(localStorage.getItem(FAVOURITES))
+  //     : [];
+  // }
+
+initializeSuperHeroApp();
+
